fb1b51c5d7509281eada4d4bfed338ad0aeff355
aleksejhanykov85 <105378393+aleksejhanykov85@users.noreply.github.com>
2025-06-10 23:54:40 +0700

    with Gemini

--- app/generate.py
+++ app/generate.py
@@ -1,11 +1,10 @@
 from openai import AsyncOpenAI
-from config import AI_Token
+from config import AI_TOKEN
 
 client = AsyncOpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key=AI_Token,
+  api_key=AI_TOKEN,
 )
-
 
 async def ai_generate(text: str):
   completion = await client.chat.completions.create(
